c39efaf6c4bb8f96ba29ea3dfad3aa65a0376a50
Artur Lavrov <arturstylus@gmail.com>
2026-06-28 20:17:49 +0200

    small clen-up

--- apps/api/app/modules/employees/events/events.py
+++ apps/api/app/modules/employees/events/events.py
@@ -2,10 +2,3 @@ class EmployeeDataChanged:
     id: str
     name: str
     email:str
-
-
-class EmployeeCreated:
-    id: str
-    name: str
-    email: str
-    title: str
--- apps/api/app/modules/incidents/events/event_handlers.py
+++ apps/api/app/modules/incidents/events/event_handlers.py
@@ -11,9 +11,3 @@ async def on_employee_data_changed(event: EmployeeDataChanged) -> None:
             employee_info.name = event.name
             employee_info.email = event.email
             db.update_employee_info(employee_info)
-
-@Mediator.handler
-async def on_employee_created(event: EmployeeCreated) -> None:
-    if event is not None:
-        employee = Employee(id=event.id, name=event.name, email=event.email)
-        db.add_employee(employee)
